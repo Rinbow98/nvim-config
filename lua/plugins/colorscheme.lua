c63014c0fdad2783180a1f45f1d932a4425f17f5
Rinbow <ray123546@gmail.com>
2025-03-22 15:08:36 +0800

    add smear_cursor

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,21 @@
 return {
   -- add colorschemes
   { "rebelot/kanagawa.nvim" },
-  { "navarasu/onedark.nvim" },
-  { "projekt0n/github-nvim-theme" },
-  { "Mofiqul/vscode.nvim" },
-  { "EdenEast/nightfox.nvim" },
+  -- { "navarasu/onedark.nvim" },
+  -- { "projekt0n/github-nvim-theme" },
+  -- { "Mofiqul/vscode.nvim" },
+  -- { "EdenEast/nightfox.nvim" },
 
   -- Configure LazyVim to load
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "kanagawa",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
     },
   },
 }
